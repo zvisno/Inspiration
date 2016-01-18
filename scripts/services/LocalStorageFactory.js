2c734df5c992fd5b0e29b10eb6b135be02357c06
zvisno <yuliya.b@example.com>
2016-01-18 17:25:32 +0100

    Fix alert width, tooltip, demo

--- scripts/services/LocalStorageFactory.js
+++ scripts/services/LocalStorageFactory.js
@@ -26,11 +26,12 @@ inspirationApp.factory('LocalStorageFactory', ['$localStorage', function($localS
     }
     
     var testLocalStorageData = [
-        "http://protruskavets.org.ua/protrusk/wp-content/gallery/divchyna-zhovtnya-2012/1.jpg",
-        "http://img0.joyreactor.cc/pics/post/%D0%9C%D0%BE%D1%8F-%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D0%B0-%D1%84%D1%8D%D0%BD%D0%B4%D0%BE%D0%BC%D1%8B-%D0%94%D1%96%D0%B2%D1%87%D0%B8%D0%BD%D0%B0-%D0%B2%D0%B8%D1%88%D0%B8%D0%B2%D0%B0%D0%BD%D0%BA%D0%B0-2580494.jpeg",
-        "http://spy-football.com/wp-content/gallery/neymar_lenzi/10-1-610x610.jpg",
-        "http://skifiya.fotoclub.info/-imgs/fest/photo/size1/img_1299435922_f49e5.jpg",
-        "http://litclub.org.ua/media/textpict/26359"]
+        "http://www.car-brand-names.com/wp-content/uploads/2015/05/Renault-logo-2.png",
+        "https://upload.wikimedia.org/wikipedia/en/thumb/2/24/Alfa_Romeo.svg/1024px-Alfa_Romeo.svg.png",
+        "http://www.toyota-global.com/showroom/emblem/passion/images/passion_img01.jpg",
+        "http://www.allcarbrandslist.com/wp-content/uploads/2015/03/mercedes-logo.jpg",
+        "http://www.bugatti.com/typo3conf/ext/bugatti_design/Resources/Public/Images/og_bugatti.jpg",
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/4/44/Hyundai_Motor_Company_logo.svg/2000px-Hyundai_Motor_Company_logo.svg.png"]
 
     
     return { getImages: getImages,
